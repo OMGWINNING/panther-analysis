95dbbb990ad8364a9b87b8abed75a462bd4216c8
lindy.sherrill <lindy.sherrill@alchemy.com>
2025-07-10 16:13:56 -0600

    removed alchemy sigma rules and fixed formatting in script

--- rules/alchemy_custom_rules/gen00003_privesc_github_patgranted_prod.py
+++ rules/alchemy_custom_rules/gen00003_privesc_github_patgranted_prod.py
@@ -5,16 +5,11 @@ LEGACY_ACTION = "org_credential_authorization.grant"
 OAUTH_TYPE = "Personal Access Token"
 
 
-
 def rule(event):
-    #Checks if action is fine-grained token
     does_action_match: bool = event.get("action") == FINE_GRAINED_ACTION
-
-    #Checks if action is a legacy token, both must be true
     does_legacy_action_match: bool = event.get("action") == LEGACY_ACTION
     does_oauth_type_match: bool = event.get("oauth_credential_type") == OAUTH_TYPE
 
-    #If Pat is fined grained or legacy token, reutrn true
     if does_action_match or (does_legacy_action_match and does_oauth_type_match):
         return True
     return False
