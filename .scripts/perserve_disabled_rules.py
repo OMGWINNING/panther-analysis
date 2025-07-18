fdf641e389911c1ea766ece166b73a49ba20bc9e
lindy.sherrill <lindy.sherrill@alchemy.com>
2025-07-18 14:53:57 -0600

    fix release push in python script

--- .scripts/perserve_disabled_rules.py
+++ .scripts/perserve_disabled_rules.py
@@ -18,13 +18,27 @@ def read_yaml_and_get_disabled_rules(file_path):
         if data.get("Enabled") == False:
             print(file_path)
 
+def get_updated_panther_analysis_rules(src, dst):
+    exclusions = {".git"}
+
+    for item in os.listdir(src_dir):
+        if item in exclusions:
+            continue
+
+        src_path = os.path.join(src_dir, item)
+        dst_path = os.path.join(dst_dir, item)
+
+        if os.path.isdir(src_path):
+            shutil.copytree(src_path, dst_path, dirs_exist_ok=True)
+        else:
+            shutil.copy2(src_path, dst_path)
 
 def main():
     rules = get_rules("./rules/")
     for rule in rules:
         read_yaml_and_get_disabled_rules(rule)
 
-    shutil.copytree("./panther-analysis-latest-release/", "./", dirs_exist_ok=True)
+    get_updated_panther_analysis_rules("./panther-analysis-latest-release/", "./")
 if __name__ == "__main__":
     main()
 
